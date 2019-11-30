405ddf2113ac22e85c4608ef5b577f58fddd695d
carolrosatto <carolrosatto@gmail.com>
2019-11-30 14:47:03 -0300

    mdlinks function running ok now

--- cli.js
+++ cli.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
+
 const mdLinks = require('./lib/index');
 
 mdLinks()
 .then((result) => console.log(result))
-.catch((error) => console.log(error))
+.catch((err) => console.log(err))
